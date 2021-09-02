2bb1b1fc46604fb118e9c0df4df5b724c12dd1b1
Dayook <lidomex@gmail.com>
2021-09-02 16:53:56 +0900

    add boiler plate and upload form

---
+++ README.md
@@ -0,0 +1,2 @@
+# clone-youtube
+리액트, node.js를 이용한 유튜브 클론코딩
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dbdbdip:1234@cloneyoutube.e9jrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
